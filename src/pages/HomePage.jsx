481e4b3c9b314e1dfc85fad08f3c59b1be78afd5
znad1213 <znad12135@gmail.com>
2025-05-31 02:53:51 +0300

    Updated HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">Welcome</h1>
+      <p className="text-lg text-gray-700">
+        This is the home page of our application.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
